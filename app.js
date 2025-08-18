9224cf4a36be43868e47b497f28da343e56fca6a
Gustavo_25 <gustavo.h.n.godoi@gmail.com>
2025-08-18 17:28:42 -0300

    criando pasta de imagens e arquivo js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+    let input = document.getElementById('nomeAmigo');
+    let nome = input.value.trim();
+
+    if (nome === "") {
+        alert("Por favor, insira um nome válido!");
+        return;
+    }
+
+    ley li = document.createElement("li");
+    li.textContent = nome;
+
+    let lista = document.getElementById("listaAmigos");
+    lista.appendChild(li);
+
+    input.value = "";
+    input.foucus();
+}
+
