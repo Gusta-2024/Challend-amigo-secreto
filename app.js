b3548619c0f6accfcc4c05be7adec993693c1250
Gustavo_25 <gustavo.h.n.godoi@gmail.com>
2025-08-18 17:50:26 -0300

    teste -01 de comportamento

--- app.js
+++ app.js
@@ -1,22 +1,54 @@
 
-let amigos = [];
+let amigos = []; /*array que ira armazenar os nomes dos amigos*/
 
+/*adicionando os nomes dos amigos*/
 function adicionarAmigo() {
     let input = document.getElementById('nomeAmigo');
     let nome = input.value.trim();
 
+/*validação que evitar o processamento de campos em vazio*/
     if (nome === "") {
         alert("Por favor, insira um nome válido!");
         return;
     }
+/*função para evitar a inserção de nomes duplicados*/
 
-    ley li = document.createElement("li");
-    li.textContent = nome;
+    if (amigos.includes(nome)) {
+        alert("Esse nome já foi adicionado");
+        return;
+    }
 
-    let lista = document.getElementById("listaAmigos");
-    lista.appendChild(li);
+    /* Adiciona à lista*/
+    amigos.push(nome);
+    atualizarLista();
 
+    /* Limpa o campo*/
     input.value = "";
-    input.foucus();
+    input.focus();
 }
+
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; /*Limpa a lista antes de atualizar*/
+
+    amigos.forEach((amigo) => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert("Adicione pelo menos dois amigos para sortear!");
+        return;
+    }
+
+    let indiceSorteado = Math.floor(Math.random() * amigos.length);
+    let sorteado = amigos[indiceSorteado];
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = "<li>🎉 O amigo secreto sorteado foi: <strong>${sorteado}</strong></li>";
+}
+
 
